7c4adcc951467b6d4e165c3b526282127a8e0a7f
almei <almei@192.168.100.4>
2020-05-19 11:53:01 -0300

    Login com pagina customizada

--- scel/src/main/java/com/fatec/scel/controller/HomeController.java
+++ scel/src/main/java/com/fatec/scel/controller/HomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
+	@GetMapping("/login")
+	public String login() {
+		return "/paginaLogin";
+	}
+
 	@GetMapping("/")
 	public String home() {
 		return "/menu";
